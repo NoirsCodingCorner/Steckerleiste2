ec5680067375876743204d125d2879729713c5d1
NoirsCodingCorner <b.kotas@web.de>
2025-04-28 11:11:46 +0200

    Initialer Commit: MovementSensor-Treiber hinzufügen
    – Hinzufügen von MovementSensor.hpp und MovementSensor.cpp
    – Implementierung eines Bewegungssensors mit Entprell-(Debounce-)Logik
    – Single-Poll-Messung mit Wieder­auslö­sper­sperre (DETECTION_HOLD_TIME_MS)
    – Doxygen-Kommentare
    – Setup via begin() und digitalRead-Auswertung in measureMovement()
    
    -Franz

--- src/ATTiny/tinymain.cpp
+++ src/ATTiny/tinymain.cpp
@@ -1,4 +1,4 @@
-#include "MovementSensor.h"
+#include "MovementSensor.hpp"
 MovementSensor sensor = MovementSensor();
 
 // This function will be called when a master requests data.
@@ -13,10 +13,11 @@ void onRequest() {
 
 void setup() {
     Serial.begin(115200);
-    sensor.begin();
-    
+    //Default I2C address of the movement sensor.
+    static constexpr uint8_t I2C_ADDRESS = 0x5C;
+
     // Initialize I²C as a slave device with the sensor's address.
-    Wire.begin(sensor.address);
+    Wire.begin(I2C_ADDRESS);
     // Register the callback function that sends sensor data when requested.
     Wire.onRequest(onRequest);
 }
